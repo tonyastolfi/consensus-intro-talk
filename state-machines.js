a28d6f350529f60bcc36f4b24e73f7be19000e06
tony.astolfi <tony.astolfi@tamr.com>
2020-06-16 15:06:54 -0400

    Build state machine visuals step by step

--- state-machines.js
+++ state-machines.js
@@ -11,7 +11,10 @@ for (i=0; i<100; ++i) {
   let k = keys[Math.floor(keys.length * Math.random())];
   let v = vals[Math.floor(vals.length * Math.random())];
   state[k] = [v, i];
-  $('#log').append("<div class='log_slot'><div class='message'>" + k.toString() + ": " + v.toString() + "</div><div class='slot_label'>slot_" + i.toString() + "</div></div>");
+  $('#log').append("<div class='log_slot' id='slot_" + i.toString() +
+                   "'><div class='message'>" + k.toString() + ": " +
+                   v.toString() + "</div><div class='slot_label'>slot_" +
+                   i.toString() + "</div></div>");
   log.push({key: k, val: v});
   states.push({...state});
   log_frames.push({
@@ -21,8 +24,14 @@ for (i=0; i<100; ++i) {
   });
 }
 
+let step_delay = 1000;
+let feed_delay = 3000;
+
 function update_state() {
-  let lsn = Math.floor(animation.progress+0.3);
+  let feed_progress = feed_delay * 100 / animation.duration;
+  console.log('feed_progress: ' + feed_progress);
+  console.log('progress: ' + animation.progress);
+  let lsn = Math.floor((animation.progress - feed_progress) * animation.duration / (animation.duration - feed_delay) + 0.3);
   console.log('update called: ' + lsn);
   let src = 
       "<table class='kvstore'><thead><td>Key</td><td>Value</td><td>LastModified</td></thead>";
@@ -35,18 +44,45 @@ function update_state() {
   document.getElementById("statemachine").innerHTML = src;
 }
 
+$("#statemachine").css({opacity: 0});
+$(".slot_label").css({opacity: 0});
+$(".log_slot").css({opacity: 0});
+$("#slot_0").css({opacity: 1});
+
 let prev_lsn = 0;
-let animation = 
-  anime({
-    targets: '#log',
-    keyframes: log_frames,
-    update: function() {
-      update_state();
-      if (!scrubbing) {
-        progress_slider.value = animation.progress;
-      }
-    },
-  });
+let animation = anime.timeline({
+      update: function() {
+        update_state();
+        if (!scrubbing) {
+          progress_slider.value = animation.progress;
+        }
+      },
+})
+    .add({
+      targets: '.slot_label',
+      easing: "linear",
+      opacity: 1,
+      duration: 150,
+    }, 1.5 * step_delay)
+    .add({
+      targets: '#statemachine',
+    easing: "linear",
+      opacity: 1,
+      duration: 150,
+    }, 3 * step_delay)
+    .add({
+      targets: '#log',
+      keyframes: log_frames,
+    }, feed_delay);
+
+for (var i=1; i<100; ++i) {
+  animation.add({
+    targets: '#slot_' + i.toString(),
+    easing: "linear",
+    opacity: 1,
+    duration: 150,
+  }, i * 25);
+}
 
 animation.pause();
 update_state();
